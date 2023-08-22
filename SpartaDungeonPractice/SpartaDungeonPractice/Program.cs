897c79aa4463647345ff5285ebbee4b992f1b5c2
Munch310 <ghkdlsi@naver.com>
2023-08-22 18:48:05 +0900

    상점 구현, 콘솔창 크기 고정
    - 상점 구현
    1. 상점 아이템 목록 구현
    2. 아이템 구매 / 판매 구현
    
    - 콘솔창 크기 고정

--- SpartaDungeonPractice/SpartaDungeonPractice/Program.cs
+++ SpartaDungeonPractice/SpartaDungeonPractice/Program.cs
@@ -9,6 +9,8 @@
 
         static void Main(string[] args)
         {
+            // witdht, height
+            Console.SetWindowSize(200, 50);
             InitItemDatabase();
             PlayerDataSet();
             MainGameScene();
@@ -16,11 +18,13 @@
 
         static void InitItemDatabase()
         {
-            _itemsInDatabase.Add(new ItemData(0, "낡은 검", 2, 0, "쉽게 볼 수 있는 낡은 검입니다."));
-            _itemsInDatabase.Add(new ItemData(1, "천 갑옷", 0, 2, "질긴 천을 덧대어 제작한 낡은 갑옷입니다."));
-            _itemsInDatabase.Add(new ItemData(2, "헤라클레스의 곤봉", 5, 0, "이 곤봉은 12가지 과업을 대비해서 갖고 다녀야합니다."));
-            _itemsInDatabase.Add(new ItemData(3, "포세이돈의 삼지창", 10, 0, "이 삼지창을 쥐면 바다를 다스릴 수 있다는 소문 때문에 선원들이 탐내는 무기입니다."));
-            _itemsInDatabase.Add(new ItemData(4, "헤르메스 트리스메기투스의 지팡이", 30, 0, "미지의 세계, 아틀란티스로 갈 수 있는 열쇠입니다."));
+            _itemsInDatabase.Add(new ItemData(0, "낡은 검", 2, 0, "쉽게 볼 수 있는 낡은 검입니다.", 200, true));
+            _itemsInDatabase.Add(new ItemData(1, "천 갑옷", 0, 2, "질긴 천을 덧대어 제작한 낡은 갑옷입니다.", 200, true));
+            _itemsInDatabase.Add(new ItemData(2, "헤라클레스의 곤봉", 5, 0, "이 곤봉은 12가지 과업을 대비해서 갖고 다녀야합니다.", 500, false));
+            _itemsInDatabase.Add(new ItemData(3, "포세이돈의 삼지창", 10, 0, "이 삼지창을 쥐면 바다를 다스릴 수 있습니다.", 1000, false));
+            _itemsInDatabase.Add(new ItemData(4, "무한의 검", 15, 0, "끝없는 힘을 가진 검", 1500, false));
+            _itemsInDatabase.Add(new ItemData(5, "신령의 갑옷", 0, 15, "신비한 힘이 깃든 갑옷", 1500, false));
+            _itemsInDatabase.Add(new ItemData(6, "트리스메기투스의 지팡이", 30, 0, "미지의 세계, 아틀란티스로 갈 수 있는 열쇠입니다.", 3000, false));
         }
 
         static void PlayerDataSet()
@@ -56,8 +60,9 @@
             Console.WriteLine("0. 게임 종료");
             Console.WriteLine("1. 상태 보기");
             Console.WriteLine("2. 인벤토리");
+            Console.WriteLine("3. 상점");
             Console.WriteLine(" ");
-            int input = CheckValidAction(0, 2);
+            int input = CheckValidAction(0, 3);
 
             switch (input)
             {
@@ -70,7 +75,98 @@
                 case 2:
                     DisplayPlayerInventory();
                     break;
+                case 3:
+                    DisplayItemShop();
+                    break;
             }
+        }
+        static void DisplayItemShop()
+        {
+            Console.Clear();
+            Console.Title = "상점";
+            Console.WriteLine("[보유 골드]\n");
+            Console.WriteLine($"{_playerStat.Gold} G\n");
+            Console.WriteLine("[상점 아이템 목록]\n");
+
+            for (int i = 0; i < _itemsInDatabase.Count; i++)
+            {
+                ItemData _shopItem = _itemsInDatabase[i];
+                string _itemName = FormatAndPad(_shopItem.ItemName, 17);
+                string _itemComm = FormatAndPad(_shopItem.ItemComm, 40);
+
+                if (!_shopItem.IsPlayerOwned)
+                {
+                    Console.WriteLine($"{i + 1} | {_itemName} | {_itemComm} | 구매 가격 : {_shopItem.ItemPrice} G");
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("[인벤토리 아이템 목록]\n");
+
+            for (int i = 0; i < _itemsInDatabase.Count; i++)
+            {
+                ItemData _inventoryItem = _itemsInDatabase[i];
+                if (_inventoryItem.IsPlayerOwned)
+                {
+                    Console.WriteLine($"{i + 1} | {_inventoryItem.ItemName} | 판매 가격 :{_inventoryItem.ItemPrice * 0.8} G | 소지중");
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("0. 나가기\n");
+
+            Console.WriteLine("아이템의 고유 번호를 입력해주세요.");
+            int _input = CheckValidAction(0, _itemsInDatabase.Count);
+
+            if (_input == 0)
+            {
+                Console.Clear();
+                MainGameScene();
+            }
+            else
+            {
+                // 입력한 번호에 해당하는 아이템의 인덱스 계산
+                int _itemIndex = _input - 1;
+
+                if (!_itemsInDatabase[_itemIndex].IsPlayerOwned)
+                {
+                    BuyItem(_itemIndex); // 아이템을 구매
+                }
+                else
+                {
+                    Sell_Item(_itemIndex); // 아이템을 판매
+                }
+                // 상점 목록을 다시 출력
+                DisplayItemShop();
+            }
+        }
+
+        static void BuyItem(int _itemIndex)
+        {
+            ItemData _selectedShopItem = _itemsInDatabase[_itemIndex];
+
+            if (_playerStat.Gold >= _selectedShopItem.ItemPrice)
+            {
+                _playerStat.Gold -= _selectedShopItem.ItemPrice;
+                _selectedShopItem.IsPlayerOwned = true;
+            }
+            else
+            {
+                Console.WriteLine("골드가 부족합니다.");
+            }
+            DisplayItemShop(); // 상점 목록을 다시 출력해줌
+        }
+
+        static void Sell_Item(int _itemIndex)
+        {
+            ItemData _selectedShopItem = _itemsInDatabase[_itemIndex];
+            if (_selectedShopItem.IsPlayerOwned == true)
+            {
+                double _sellRet = _selectedShopItem.ItemPrice * 0.8;
+                _playerStat.Gold += (int)_sellRet;
+                _selectedShopItem.IsPlayerOwned = false;
+            }
+
+            DisplayItemShop();
         }
 
         static void DisplayPlayerInventory()
@@ -86,16 +182,26 @@
             {
                 ItemData _item = _itemsInDatabase[i];
 
+                if (!_item.IsPlayerOwned)
+                {
+                    continue; // IsPlayerOwned가 false면 출력하지 않고 다음 아이템으로 넘어감
+                }
+
                 _itemEquipped = _item.IsItemEquipped ? "[E] " : "";
+
+                string _itemName = FormatAndPad(_item.ItemName, 17);
+                string _itemComm = FormatAndPad(_item.ItemComm, 30);
+
+                Console.Write($"{_item.ItemId + 1} | ");
                 if (_item.IsItemEquipped)
                 {
                     SetConsoleColor(ConsoleColor.Yellow);
                 }
                 Console.Write($"{_itemEquipped}");
                 Console.ResetColor();
-                Console.Write($"{_item.ItemName}");
+                Console.Write($"{_itemName}");
                 DisplayAtkOrDef(_item);
-                Console.WriteLine($" {_item.ItemComm} ");
+                Console.WriteLine($" {_itemComm} ");
             }
             Console.WriteLine(" ");
             Console.WriteLine("1. 장착 관리");
@@ -115,7 +221,7 @@
                     break;
             }
         }
-
+        
         static void ManagementPlayerInventory()
         {
             Console.Clear();
@@ -127,16 +233,25 @@
             for (int i = 0; i < _itemsInDatabase.Count; i++)
             {
                 ItemData _item = _itemsInDatabase[i];
+
+                if (!_item.IsPlayerOwned)
+                {
+                    continue; // IsPlayerOwned가 false면 출력하지 않고 다음 아이템으로 넘어감
+                }
+
                 _itemEquipped = _item.IsItemEquipped ? "[E] " : "";
+                string _itemName = FormatAndPad(_item.ItemName, 17);
+                string _itemComm = FormatAndPad(_item.ItemComm, 30);
+                Console.Write($"{_item.ItemId + 1} | ");
                 if (_item.IsItemEquipped)
                 {
                     SetConsoleColor(ConsoleColor.Yellow);
                 }
                 Console.Write($"{_itemEquipped}");
                 Console.ResetColor();
-                Console.Write($"{_item.ItemId + 1}.{_item.ItemName}");
+                Console.Write($"{_itemName}");
                 DisplayAtkOrDef(_item);
-                Console.WriteLine($" {_item.ItemComm} ");
+                Console.WriteLine($" {_itemComm} ");
             }
             Console.WriteLine(" ");
             Console.WriteLine("0. 나가기");
@@ -152,6 +267,22 @@
                 ItemData _selectedItem = _itemsInDatabase[_input - 1];
                 ToggleEquip(_selectedItem);
                 ManagementPlayerInventory();
+            }
+        }
+
+        static string FormatAndPad(string _text, int _width)
+        {
+            int _remainingSpace = _width - _text.Length;
+            if (_remainingSpace <= 0)
+            {
+                return _text;
+            }
+            else
+            {
+                int _leftPadding = _remainingSpace / 2;
+                int _rightPadding = _remainingSpace - _leftPadding;
+                string _formattedText = new string(' ', 2 * _leftPadding) + _text + new string(' ', 2 * _rightPadding);
+                return _formattedText;
             }
         }
 
@@ -180,9 +311,6 @@
             {
                 _playerEquippedItems.Remove(_item);
             }
-            // 만약 이미 장착한 아이템이 있다면? -> ItemAtk
-            
-
             UpdatePlayerStats();
         }
 
@@ -205,11 +333,13 @@
         {
             if (_item.ItemAtk > 0 && _item.ItemDef == 0)
             {
-                Console.Write($"| 공격력 + {_item.ItemAtk} |");
+                string _itemAtk = FormatAndPad(_item.ItemAtk.ToString(), 3);
+                Console.Write($"| 공격력 + {_itemAtk} |");
             }
             else if (_item.ItemAtk == 0 && _item.ItemDef > 0)
             {
-                Console.Write($"| 방어력 + {_item.ItemDef} |");
+                string _itemDef = FormatAndPad(_item.ItemDef.ToString(), 3);
+                Console.Write($"| 방어력 + {_itemDef} |");
             }
         }
 
@@ -318,19 +448,23 @@
     {
         public int ItemId;
         public bool IsItemEquipped;
+        public bool IsPlayerOwned;
         public string ItemName;
         public int ItemAtk;
         public int ItemDef;
         public string ItemComm;
+        public int ItemPrice { get; set; }
 
-        public ItemData(int _itemId, string _itemName, int _itemAtk, int _itemDef, string _itemComm)
+        public ItemData(int _itemId, string _itemName, int _itemAtk, int _itemDef, string _itemComm, int _itemPrice, bool _isPlayerOwned)
         {
             ItemId = _itemId;
             ItemName = _itemName;
             ItemAtk = _itemAtk;
             ItemDef = _itemDef;
             ItemComm = _itemComm;
+            ItemPrice = _itemPrice;
             IsItemEquipped = false;
+            IsPlayerOwned = _isPlayerOwned;
         }
     }
 }
